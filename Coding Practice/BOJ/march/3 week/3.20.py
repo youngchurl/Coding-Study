c0220314fb4c16c5608f7b07897e6a882da7bc50
YoungChurl <95406074+youngchurl@users.noreply.github.com>
2022-03-20 23:10:08 +0900

    3.20

---
+++ Coding Practice/BOJ/march/3 week/3.20.py
@@ -0,0 +1,15 @@
+# 1. Boj 2720번 세탁소 사장 동혁
+from re import I
+
+
+li = [25, 10, 5, 1]
+
+tc = int(input())
+for _ in range(tc):
+    res = [0,0,0,0]
+    co = int(input())
+    for i in range(4):
+        n = co//li[i]
+        co %=li[i]
+        res[i] = n
+    print(*res)
